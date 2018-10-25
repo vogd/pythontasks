abc63dffc3bde78bddebd02951742b7131d9a583
Oleks Golovatyi <golovaty.alexander@googlemail.com>
2018-10-25 16:37:15 -0700

    Working task26

--- task26.py
+++ task26.py
@@ -13,30 +13,33 @@ def generatelist(listname):
   n+=1
  print listname
 
-generatelist(list1)
-generatelist(list2)
-generatelist(list3)
-
-while n <=2:
-  print "list1 item "+str(n) + " = "+str(list1[n])
-  print "list2 item "+str(n) + " = "+str(list2[n])
-  print "list3 item "+str(n) + " = "+str(list3[n])
-  if list1[n]==list2[n] and list2[n]==list3[n]:
-   print "player won by horizontal" 
-   #raise SystemExit
-  n+=1  
-if list1[0]==list2[1] and list2[1]==list3[2]:
- print "player won by diagonal from left to right"
- raise SystemExit
-if list3[0]==list2[1] and list2[1]==list1[2]:
- print "player won by diagonal from right to left"
- raise SystemExit
-else:
+def main():
  list1=[]
  list2=[]
  list3=[]
  generatelist(list1)
  generatelist(list2)
- generatelist(list3) 
+ generatelist(list3)
+ checkwinner(list1,list2,list3)
 
+def checkwinner(list1,list2,list3): 
+ print ""
+ n=0 
+ while n <=2:
+#  print "list1 item "+str(n) + " = "+str(list1[n])
+#  print "list2 item "+str(n) + " = "+str(list2[n])
+#  print "list3 item "+str(n) + " = "+str(list3[n])
+  if list1[n]==list2[n] and list2[n]==list3[n]:
+   print "player won by horizontal" 
+   raise SystemExit
+  n+=1  
+ if list1[0]==list2[1] and list2[1]==list3[2]:
+  print "player won by diagonal from left to right"
+  raise SystemExit
+ if list3[0]==list2[1] and list2[1]==list1[2]:
+  print "player won by diagonal from right to left"
+  raise SystemExit
+ else:
+  main() 
 
+main()
