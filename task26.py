4bf9d6a45a79f192c29ef7a01d526b5b7321a5c3
Oleks Golovatyi <golovaty.alexander@googlemail.com>
2018-10-25 14:21:44 -0700

    Add logic to check if the player won after lists generated one more time in a loop

--- task26.py
+++ task26.py
@@ -7,7 +7,6 @@ list3=[]
 
 
 def generatelist(listname):
- #global n
  n=0
  while n<=2:
   listname.append(randrange(0,3))
@@ -22,4 +21,22 @@ while n <=2:
   print "list1 item "+str(n) + " = "+str(list1[n])
   print "list2 item "+str(n) + " = "+str(list2[n])
   print "list3 item "+str(n) + " = "+str(list3[n])
-  n+=1 
+  if list1[n]==list2[n] and list2[n]==list3[n]:
+   print "player won by horizontal" 
+   #raise SystemExit
+  n+=1  
+if list1[0]==list2[1] and list2[1]==list3[2]:
+ print "player won by diagonal from left to right"
+ raise SystemExit
+if list3[0]==list2[1] and list2[1]==list1[2]:
+ print "player won by diagonal from right to left"
+ raise SystemExit
+else:
+ list1=[]
+ list2=[]
+ list3=[]
+ generatelist(list1)
+ generatelist(list2)
+ generatelist(list3) 
+
+
