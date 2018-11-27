f67a704f718ff83d82fdd512b5cec1746b285f00
Oleks Golovatyi <golovaty.alexander@googlemail.com>
2018-11-27 09:43:51 -0800

    task27 winner check working

--- task27.py
+++ task27.py
@@ -1,16 +1,13 @@
 from random import randrange
 
-i=0
 n=0
+k=0
 symbol=0
 coord=''
 coordlist=[]
 list1=[]
 list2=[]
 list3=[]
-available1=[]
-available2=[]
-available3=[]
 winner=0
 
 def generatelist(listname):
@@ -23,7 +20,60 @@ def generatelist(listname):
 player1=1
 player2=0
 
+def checkmoves(list1,list2,list3):
+  # Match lists and find if same position items in lists are the same (check by vertical)
+  global k
+  for k in list1:
+   if k <> '':
+    if k==0:
+     winner=1 
+    else: 
+     winner=2
+    if k in list2 and k in list3:
+     print "Winner is player "+ str(winner)+ " !!!"
+     raise SystemExit
 
+# Check winner if All items in list1 are filled with values   
+    if len(set(list1))==1: 
+     print "Winner is player "+ str(winner)+ " !!!"
+     raise SystemExit
+
+# Check winner if All items in list2 are filled with values
+  for k in list2:
+   if k <> '':
+    if k==0:
+     winner=1 
+    else: 
+     winner=2
+    if len(set(list2))==1: 
+     print "Winner is player "+ str(winner)+ " !!!"
+     raise SystemExit
+
+# Check winner if All items in list3 are filled with values
+  for k in list3: 
+   if k <> '':
+    if k==0:
+     winner=1 
+    else: 
+     winner=2
+    if len(set(list3))==1: 
+     print "Winner is player "+ str(winner)+ " !!!"
+     raise SystemExit
+
+#Check winner by diagonal
+  if list1[0]<>''and list2[1]<>'' and list3[2]<>'':
+   if str(list1[0])==str(list2[1]) and str(list2[1])==str(list3[2]):
+      if str(list1[0])=='1':
+       print "Winner is player2 !!!"
+      else:
+       print "Winner is player1 !!!"
+  if list3[0]<>'' and list2[1]<>'' and list1[2]<>'':      
+   if str(list3[0])==str(list2[1]) and str(list2[1])==str(list1[2]):
+      if str(list3[0])=='1':
+       print "Winner is player2 !!!"
+      else:
+       print "Winner is player1 !!!"      
+    
 def readmove(player):
  coord=raw_input("Input player "+str(player)+" move coordinates x,y : ")
  coordlist=list(coord)  
@@ -34,40 +84,36 @@ def readmove(player):
   readmove(player)
  else: 
   if int(player)==1:
-   symbol==0
+   symbol=0
    print "Will call func to update list with '0'"
+   print "symbol: "+str(symbol)
    placemove(symbol,x,y,player)
   else:
-   symbol==1
+   symbol=1
    print "Will call func to update list with '1'"
+   print "symbol: "+str(symbol)
    placemove(symbol,x,y,player)
+
+
+def listappend(symbol,listname,x,player):
+ if listname[int(x)]<>0 and listname[int(x)]<>1:
+   print "appending "+str(listname)
+   listname[int(x)]=int(symbol)
+ else:
+   print "please enter new coordinates, that slot is already used"
+   readmove(player)
 
 def placemove(symbol,x,y,player):
  if int(y)==1:
-  if list1[int(x)]<>0 and list1[int(x)]<>1:
-   print "appending list1"
-   list1[int(x)]=int(symbol)
-  else:
-   print "please enter new coordinates, that slot is already used"
-   readmove(player)
+  listappend(symbol,list1,x,player)
  elif int(y)==2:
-  if list2[int(x)]<>0 and list2[int(x)]<>1:
-   print "appending list2"
-   list2[int(x)]=int(symbol)
-  else:
-   print "please enter new coordinates, that slot is already used"
-   readmove(player)
+  listappend(symbol,list2,x,player)
  elif int(y)==3:
-  if list3[int(x)]<>0 and list3[int(x)]<>1:
-   print "appending list3"
-   list3[int(x)]=int(symbol)
-  else:
-   print "please enter new coordinates, that slot is already used"
-   readmove(player)
-
+  listappend(symbol,list3,x,player)
  else:
    print "Enter Valid X cordinates from 1 to 3 one more time !!! : "
    readmove(player)
+
  print list1
  print list2
  print list3
@@ -76,8 +122,8 @@ generatelist(list1)
 generatelist(list2)
 generatelist(list3)
 
-while int(winner)<>1:
+while n<=3:
   readmove(1)
+  checkmoves(list1,list2,list3)
   readmove(2)
-
-#checklist (list1,available1)
+  checkmoves(list1,list2,list3)
